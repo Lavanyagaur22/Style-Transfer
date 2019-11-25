43f251780718cf652576fe2a47d3905d7475c655
Lavanya Gaur <lavanyagaur22@gmail.com>
2019-11-25 23:12:49 +0530

    no UI bindings

--- app/src/main/java/com/lavanya/styletransfer/MainActivity.kt
+++ app/src/main/java/com/lavanya/styletransfer/MainActivity.kt
@@ -1,12 +1,126 @@
 package com.lavanya.styletransfer
 
-import androidx.appcompat.app.AppCompatActivity
+import ai.fritz.core.Fritz
+import ai.fritz.vision.FritzVision
+import ai.fritz.vision.FritzVisionImage
+import ai.fritz.vision.ImageRotation
+import ai.fritz.vision.PredictorStatusListener
+import ai.fritz.vision.styletransfer.FritzVisionStylePredictor
+import ai.fritz.vision.styletransfer.PaintingManagedModels
+import android.graphics.Canvas
 import android.os.Bundle
+import android.util.Log
+import android.util.Size
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.*
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.concurrent.Executors
+
 
 class MainActivity : AppCompatActivity() {
+
+    private val API_KEY = "dcf6227d8ecf4968b4e1a1b5fc1c483b"
+    private val executor = Executors.newSingleThreadExecutor()
+    private val TAG = javaClass.simpleName
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /*In the MainActivity class, initialize Fritz SDK.
+       */
+        Fritz.configure(this, API_KEY)
+
+        /*Instead of calling `startCamera()` on the main thread, we use `viewFinder.post { ... }`
+         to make sure that `viewFinder` has already been inflated into the view when `startCamera()` is called.
+         */
+        view_finder.post {
+            startCamera()
+        }
+    }
+
+    //Function that creates and displays the camera preview
+    private fun startCamera() {
+
+        //Specify the configuration for the preview
+        val previewConfig = PreviewConfig.Builder()
+            .apply {
+                //Set the resolution of the captured image
+                setTargetResolution(Size(1920, 1080))
+            }
+            .build()
+
+        //Generate a preview
+        val preview = Preview(previewConfig)
+
+        //Add a listener to update preview automatically
+        preview.setOnPreviewOutputUpdateListener {
+
+            val parent = view_finder.parent as ViewGroup
+
+            //Remove thr old preview
+            parent.removeView(view_finder)
+
+            //Add the new preview
+            parent.addView(view_finder, 0)
+            view_finder.surfaceTexture = it.surfaceTexture
+        }
+
+        val analyzerConfig = ImageAnalysisConfig.Builder().apply {
+            // In our analysis, we care more about the latest image than
+            // analyzing *every* image
+            setImageReaderMode(
+                ImageAnalysis.ImageReaderMode.ACQUIRE_LATEST_IMAGE
+            )
+        }.build()
+
+        val imageAnalysis = ImageAnalysis(analyzerConfig).apply {
+            setAnalyzer(executor, ImageProcessor())
+        }
+
+        /*  Bind use cases to lifecycle. If Android Studio complains about "this"
+            being not a LifecycleOwner, try rebuilding the project or updating the appcompat dependency to
+            version 1.1.0 or higher.
+         */
+        CameraX.bindToLifecycle(this, preview, imageAnalysis)
+    }
+
+    /* It allows us to define a custom class implementing the ImageAnalysis.Analyzer interface,
+      which will be called with incoming camera frames.
+   */
+    inner class ImageProcessor : ImageAnalysis.Analyzer {
+        var predictor: FritzVisionStylePredictor? = null
+        val TAG = javaClass.simpleName
+
+        override fun analyze(image: ImageProxy?, rotationDegrees: Int) {
+
+            //Handle all the ML logic here
+            val mediaImage = image?.image
+
+            val imageRotation = ImageRotation.getFromValue(rotationDegrees)
+
+            val visionImage = FritzVisionImage.fromMediaImage(mediaImage, imageRotation)
+
+            val managedModel = PaintingManagedModels.PINK_BLUE_RHOMBUS_MANAGED_MODEL
+
+            FritzVision.StyleTransfer.loadPredictor(
+                managedModel, object : PredictorStatusListener<FritzVisionStylePredictor> {
+                    override fun onPredictorReady(stylePredictor: FritzVisionStylePredictor?) {
+                        Log.d(TAG, "Style Transfer predictor is ready")
+                        predictor = stylePredictor
+                    }
+                }
+            )
+
+
+            val styleResult = predictor?.predict(visionImage)
+
+            runOnUiThread {
+                Log.d(TAG, "UI thread")
+
+//                styleResult?.drawToCanvas(Canvas())
+            }
+        }
     }
 }
